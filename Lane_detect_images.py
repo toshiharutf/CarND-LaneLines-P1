52f7e89d8ed736a67ea0326976e53da113c48ee1
toshiharutf <rttf06@gmail.com>
2017-07-18 02:23:29 -0500

    Finished the LaneLines program for static images

--- Lane_detect_images.py
+++ Lane_detect_images.py
@@ -17,20 +17,16 @@ import glob, os
 #    if file.endswith(".jpg"):
 #        print(os.path.join("/test_images", file))
         
-
-def init(imagesFolder):
-    # Read in and grayscale the image
-    os.chdir(imagesFolder)
-
-
 def grayscale(img):
     # Grayscale transform
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 def colorFilter(img,bgr,thresh):
-    # Isolate certain color range of image
-    # To isolate multiple colors, isolate them separately and bitwise added them
-    # color is assumed in RGB format
+    """
+    Isolate certain color range of image
+    To isolate multiple colors, isolate them separately and bitwise added them
+    color is assumed in RGB format
+    """
     minBGR = np.array([bgr[0] - thresh, bgr[1] - thresh, bgr[2] - thresh])
     maxBGR = np.array([bgr[0] + thresh, bgr[1] + thresh, bgr[2] + thresh])
     return cv2.inRange(img,minBGR,maxBGR)
@@ -46,31 +42,40 @@ def gaussianBlur(img, kernel_size):
 def roi(img, vertices):
     """
     Applies an image mask.
-    
-    Only keeps the region of the image defined by the polygon
+        Only keeps the region of the image defined by the polygon
     formed from `vertices`. The rest of the image is set to black.
     """
-    #defining a blank mask to start with
+    """defining a blank mask to start with"""
     mask = np.zeros_like(img)   
     
-    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    """defining a 3 channel or 1 channel color to fill the mask with depending
+    on the input image
+    """
     if len(img.shape) > 2:
         channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
         ignore_mask_color = (255,) * channel_count
     else:
         ignore_mask_color = 255
         
-    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    """filling pixels inside the polygon defined by "vertices" with the
+    fill color    
+    """
     cv2.fillPoly(mask, vertices, ignore_mask_color)
     plt.figure()
     plt.imshow(image)
     plt.contour(mask,colors='b',linestyles='dashed')
     
-    #returning the image only where mask pixels are nonzero
+    """returning the image only where mask pixels are nonzero"""
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
 def Lane_lines_fit(houghLines,poly_degree=1):
+    """
+    The points obtained from the Hough transformation are used to
+    identify the polynomial curve parameters using the function polyfit
+    The outputs of the Lane_lines_fit function are the left and right
+    curve 
+    """
     leftPoints  =[[],[]]
     rightPoints =[[],[]]
     leftCurve = np.zeros(shape=(1,2))
@@ -85,13 +90,9 @@ def Lane_lines_fit(houghLines,poly_degree=1):
                rightPoints=np.append(rightPoints,[[x1,x2],[y1,y2]],axis=1)
     
 #    print(rightPoints)
-    leftCurve=np.polyfit(leftPoints[1,:],leftPoints[0,:],poly_degree)  #x=f(y)
-    rightCurve=np.polyfit(rightPoints[1,:],rightPoints[0,:],poly_degree)  #x=f(y)
-
-#    result = []
-#    result=result.append(leftCurve)
-#    result=result.append(rightCurve)
-    
+    leftCurve=np.polyfit(leftPoints[1,:],leftPoints[0,:],poly_degree)  #    x=f(y)
+    rightCurve=np.polyfit(rightPoints[1,:],rightPoints[0,:],poly_degree)  # x=f(y)
+   
     return leftCurve,rightCurve
 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
@@ -129,7 +130,14 @@ def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
     return cv2.addWeighted(initial_img, α, img, β, λ)
 
 def drawLines(img,leftCurve,rightCurve,verLim):
-    # Left line initial and end points
+    """
+    This function takes an image, and the curve equation of the left and right
+    lanes. Then, it draws this two curves over the image. 
+
+    By default, both equation start at the bottom of the figure and finish at
+    y = verLim
+    """
+    """ Left line initial and end points"""
     imshape = img.shape
     y1_left = imshape[0]
     x1_left = int(y1_left*leftCurve[0]+leftCurve[1])
@@ -137,7 +145,7 @@ def drawLines(img,leftCurve,rightCurve,verLim):
     y2_left = verLim
     x2_left = int(y2_left*leftCurve[0]+leftCurve[1])
     
-    # Right line initial and end points
+    """ Right line initial and end points """
     y1_right = imshape[0]
     x1_right = int(y1_right*rightCurve[0]+rightCurve[1])
     
@@ -146,9 +154,14 @@ def drawLines(img,leftCurve,rightCurve,verLim):
     
     fit_line_image = np.copy(img)*0
     
+    """ Drawing the curves """
     cv2.line(fit_line_image,(x1_left,y1_left),(x2_left,y2_left),(255,0,0),10)
     cv2.line(fit_line_image,(x1_right,y1_right),(x2_right,y2_right),(255,0,0),10)
-
+    
+    """
+    Overlaying the curves on the input image, applying a previous transparency
+    transformation
+    """
     return  weighted_img(image,fit_line_image,0.8,1,0)
 
 
@@ -170,20 +183,22 @@ for file in os.listdir(imagesFolder):
         imshape = image.shape
         gray = gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
             
-        # Define a kernel size and apply Gaussian smoothing
+        """ Define a kernel size and apply Gaussian smoothing"""
         blur_gray = gaussianBlur(gray,kernel_size=5)
         
         # Define our parameters for Canny and apply
         low_threshold = 50
-        high_threshold = 150
+        high_threshold = 200
         edges = canny(blur_gray, low_threshold, high_threshold)
         
-        # Masking Region of interest ()
+        """ Masking Region of interest"""
         vertices = np.array([[(0,imshape[0]),(450, 300), (500,300), (imshape[1],imshape[0])]], dtype=np.int32)
         masked_edges = roi(edges,vertices)
         
-        # Define the Hough transform parameters
-        # Make a blank the same size as our image to draw on
+        """
+        Define the Hough transform parameters
+        Make a blank the same size as our image to draw on
+        """
         rho = 1 # distance resolution in pixels of the Hough grid
         theta = np.pi/180 # angular resolution in radians of the Hough grid
         threshold = 60  #1   # minimum number of votes (intersections in Hough grid cell)
@@ -191,20 +206,23 @@ for file in os.listdir(imagesFolder):
         max_line_gap = 15 #1   # maximum gap in pixels between connectable line segments
         line_image = np.copy(image)*0 # creating a blank to draw lines on
         
-        # Run Hough on edge detected image
+        """ Run Hough on edge detected image"""
         houghLines = hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
         #print(houghLines)
         
-        # Identified the curve equation of each, left and right lanes
+        """ Identified the curve equation of each, left and right lanes"""
         leftCurve,rightCurve = Lane_lines_fit(houghLines,poly_degree=1)
         
         verLim = 350 # Vertical limit to draw the identified lane's curves
         output_img = drawLines(image,leftCurve,rightCurve,verLim)
+        
+        #print(file)
         plt.figure()
+        plt.title(file[:-4]+'-output')
         plt.imshow(output_img)
         
         output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)  #change to RGB
-        cv2.imwrite(outputFolder+"/"+file+'-output.jpg',output_img)
+        cv2.imwrite(outputFolder+"/"+file[:-4]+'-output.jpg',output_img)
     
     
 
